c591dcd4f55de972e0a9a3620d0346086e83dc20
Bruno Brito <developer.bcbrito@gmail.com>
2022-10-19 19:14:16 -0300

    Versão Inicial Memoteca

---
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-criar-pensamento',
+  templateUrl: './criar-pensamento.component.html',
+  styleUrls: ['./criar-pensamento.component.css']
+})
+export class CriarPensamentoComponent implements OnInit {
+
+  pensamentos = []
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
